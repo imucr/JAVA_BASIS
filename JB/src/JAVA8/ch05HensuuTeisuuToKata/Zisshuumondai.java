ab231f8810cc828ec9eec4c716eda0ef9dcc93a7
LCL <crforest0301@gmail.com>
2018-01-21 16:11:19 +0900

    「Java8問題集」実習問題解決
    第２、４、５章解決

---
+++ JB/src/JAVA8/ch05HensuuTeisuuToKata/Zisshuumondai.java
@@ -0,0 +1,12 @@
+package JAVA8.ch05HensuuTeisuuToKata;
+
+public class Zisshuumondai {
+
+	public static void main(String[] args){
+		System.out.println("問題102-105　定数タイプbyteの定義範囲確認"); 
+		byte b1=-125;
+		//byte b1="あ";
+		System.out.println(b1); 
+	}
+	
+}
