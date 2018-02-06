6a831c745c915ca0140afe46e95892ed72cc7ace
LCL <crforest0301@gmail.com>
2018-02-07 00:04:33 +0900

    [fix] 残feedback反映

--- JB/src/JAVA8/ch10ClassNoKankeiWoFukameru/MyStaticImport2.java
+++ JB/src/JAVA8/ch10ClassNoKankeiWoFukameru/MyStaticImport2.java
@@ -1,12 +1,15 @@
 package JAVA8.ch10ClassNoKankeiWoFukameru;
 
 //Static Import：Static変数名にクラス名を付けずに参照出来るようになる
-
-	import static java.lang.Math.PI;
 	
 	public class MyStaticImport2 {
 	
 	  public static void main(String[] args) {
-		  System.out.println(PI); //MEMO static import使わないで「Math.PI」使用が望ましい。誤解を招く原因となるので。
+		  System.out.println(Math.PI);
+		  		/* MEMO 
+		  		  「import static java.lang.Math.PI;」使って「PI」だけで表現可。
+		  		   だが「Math.PI」使用が望ましい。さもないと誤解を招く原因となるので。
+		  		 */
+
 	  }
 }
--- JB/src/JAVA8/ch11ClassNoKeishou/SubjectExample.java
+++ JB/src/JAVA8/ch11ClassNoKeishou/SubjectExample.java
@@ -1,30 +1,29 @@
 package JAVA8.ch11ClassNoKeishou;
 
 //Overrideの例示：親クラスの処理に内容を追加する場合
-class Subject {
-	String name;
+	class Subject {
+		String name;
+		
+		void printInfo(){
+			System.out.println("科目名：" + name); 
+		}
+	}
 	
-	void printInfo(){
-		System.out.println("科目名：" + name); 
+	class MajorSubject extends Subject {
+		String targetGrader;
+		
+		void printInfo(){
+			super.printInfo();
+			System.out.println("対象学年：" + targetGrader); 
+		}
 	}
-}
-
-class MajorSubject extends Subject {
-	String targetGrader;
 	
-	void printInfo(){
-		super.printInfo();
-		System.out.println("対象学年：" + targetGrader); 
-	}
-}
-
-class SubjectExample {
-	public static void main(String args[]){
-		MajorSubject sub = new MajorSubject();
-		sub.name = "Java Programming";
-		sub.targetGrader = "2年生";
-		sub.printInfo();
-	}
-
+	class SubjectExample {
+		public static void main(String args[]){
+			MajorSubject sub = new MajorSubject();
+			sub.name = "Java Programming";
+			sub.targetGrader = "2年生";
+			sub.printInfo();
+		}
 
 }
