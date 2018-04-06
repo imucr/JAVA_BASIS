ea47e883b51d1b7adadb9675b88b8d3d9ce937f8
imucr <imucr0301@gmail.com>
2018-04-06 20:48:14 +0900

    [refactor] Interfaceのデータを列挙型に修正

--- JB/src/sougouZisshuu/sigekiJava/ch6KeishouToInterface/ToshokanSiryouKanri/Lendable.java
+++ JB/src/sougouZisshuu/sigekiJava/ch6KeishouToInterface/ToshokanSiryouKanri/Lendable.java
@@ -1,13 +1,12 @@
 package sougouZisshuu.sigekiJava.ch6KeishouToInterface.toshokanSiryouKanri;
 
-/*
-Interfaceのフィールド：
-	・Interfaceを実装するクラスがよく使う正数は、Interfaceの中に宣言するのがよい。//QUESTION 正数vs定数？
-	・Interfaceのフィールドにfinal static付けなくてもコンパイラーによって自動的に付く。
-*/
+//データとしてstatic final正数の代わりに、列挙型使用。
 interface Lendable {
-	final static byte STATE_BORROWED = 1;
-	final static byte STATE_NORMAL = 0;
+
+	enum BookState {
+		STATE_BORROWED, STATE_NORMAL
+	}
+	
 	void checkOut(String borrower, String date); //MEMO Interfaceは必ず抽象メソッドを持つので、abstractを付けなくてもコンパイラーが追加すてくれる。
 	void checkIn();
 }
--- JB/src/sougouZisshuu/sigekiJava/ch6KeishouToInterface/ToshokanSiryouKanri/SeparateVolume.java
+++ JB/src/sougouZisshuu/sigekiJava/ch6KeishouToInterface/ToshokanSiryouKanri/SeparateVolume.java
@@ -6,23 +6,25 @@ class SeparateVolume implements Lendable {
 	String writer;
 	String borrower;
 	String checkOutDate;
-	byte state;
+	BookState state;
 	
 	SeparateVolume(String requestNo, String bookTitle, String writer) {
 		this.requestNo = requestNo;
 		this.bookTitle = bookTitle;
 		this.writer = writer;
+		this.state = BookState.STATE_NORMAL;
+			//MEMO 列挙型はクラスとして取り扱われるので、デフォルト値はnull。それでstatic final正数を使う場合とは違って、初期値の必要。
 	}
 
 	@Override
 	public void checkOut(String borrower, String date) {
-		if (state != STATE_NORMAL) {
+		if (state != BookState.STATE_NORMAL) {
 			return;
 		}
 		
 		this.borrower = borrower;
 		this.checkOutDate = date;
-		this.state = STATE_BORROWED;
+		this.state = BookState.STATE_BORROWED;
 		
 		System.out.println("＊「" + bookTitle + "」が貸し出されました。");
 		System.out.println("貸出人：" + borrower);
@@ -34,7 +36,7 @@ class SeparateVolume implements Lendable {
 	public void checkIn() {
 		this.borrower = null;
 		this.checkOutDate = null;
-		this.state = STATE_NORMAL;
+		this.state = BookState.STATE_NORMAL;
 		System.out.println("＊「" + bookTitle + "」が返却されました。" + "\n");
 	}
 	
